3f7ff4c47e6508d5b9bdccfc83de065241088070
Matthias Schneller <matthias.schneller@gmail.com>
2021-05-19 18:47:10 +0200

    rename file

--- src/Raytracing.jl
+++ src/Raytracing.jl
@@ -3,8 +3,6 @@ module Raytracing
 export Free, Lense, Setup, propagation!
 export Wave
 
-abstract type AbstractWave end
-
 include("electronBeam.jl")
 include("components.jl")
 
--- src/components.jl
+++ src/components.jl
@@ -16,3 +16,18 @@ Create and return the setup struct. The varargs `comps` describe
 the different components that are used in the setup.
 """
 Setup(comps::Component...) = Setup(collect(comps))
+
+
+function propagation!(wave::Wave, setup::Setup)
+    
+    # create the setup matrix that contains all the 
+    setup_mat = setup.setup[end].mat
+
+    for i = size(setup.setup, 1)-1:-1:1
+        setup_mat *= setup.setup[i].mat
+    end
+
+    for i = 1:size(wave.x, 1)
+        wave.ψ[i] *= setup_mat
+    end
+end
