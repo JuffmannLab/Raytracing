851508bbfb82cc859c26e5a1b911bb64c8e5d49a
Matthias Schneller <matthias.schneller@gmail.com>
2021-06-25 09:58:05 +0200

    random sampling

--- src/electron.jl
+++ src/electron.jl
@@ -9,16 +9,19 @@ mutable struct Electron1d <: AbstractElectron
 end
 
 """
-    Electron(x::Vector{<:Real}, intensity::Vector{<:Real}, energy::Real)::Electron1d
+    Electron(x::Vector{<:Real}, intensity::Vector{<:Real}, energy::Real, n::Int64)::Electron1d
 
 Return a Electron1d type.
 
 Create and return the 1D wave function. The `x` value corresponds to the
 x-axis, and `intensity` corresponds to the intensity distribution along
-the given axis.
+the given axis. `n` correspond to the number of rays that should be simulated.
 """
-function Electron(x::Vector{<:Real}, intensity::Vector{<:Real}, energy::Real)::Electron1d
+function Electron(x::Vector{<:Real}, intensity::Vector{<:Real}, energy::Real, n::Int64)::Electron1d
    
+
+    # TODO:
+    # implement the random ray positions
     # create the new position angle vectors
     ψ = Vector{Array}(undef, size(x, 1))
     p_ges = similar(intensity)
@@ -41,35 +44,52 @@ end
 
 """
     Electron(x::Vector{<:Real}, y::Vector{<:Real}, intensity::Matrix{<:Real},
-             energy::Real)::Electron2d
+             energy::Real, n::Int64)::Electron2d
 
 Return the Electron2d type.
 
 Construct and return a 2D wave. The `x` and `y` vector correspond to the
 x and y axis of the problem, and the `intensity` is the intensity corresponding
 to the x and y axis given prior. The `energy` parameter denotes the electron
-energy in eV.
+energy in eV. The number of rays that should be simulated are cenoted by the `n`
+value.
 """
 function Electron(x::Vector{<:Real}, y::Vector{<:Real}, intensity::Matrix{<:Real},
-                  energy::Real)::Electron2d
+                  energy::Real, n::Int64)::Electron2d
     
     # create the array with the different angles and positions
-    ψ = Vector{Array}(undef, size(x, 1) * size(y, 1))
+    ψ = Vector{Array}(undef, n)
 
     # create a flattened intensity array
-    int_flat = similar(intensity, size(intensity, 1) * size(intensity, 2))
+    int_flat = similar(intensity, n)
     p_ges = similar(int_flat)
 
     # calculate the momentum
     momentum = sqrt( 2 * energy * q / m_e ) * m_e
 
-    # fill the ψ array
-    for i = 1:size(x, 1)
-        for j = 1:size(y, 1)
-            ψ[i+(j-1)*size(y, 1)] = [x[i], y[j], 0., 0.]
-            int_flat[i+(j-1)*size(y, 1)] = intensity[i, j]
-            p_ges[i+(j-1)*size(y, 1)] = momentum
-        end
+    # calculate the span of x and y, aswell as Δx and Δy
+    span_x = abs(x[1]-x[end])
+    span_y = abs(y[1]-y[end])
+    Δx = abs(x[2]-x[1])
+    Δy = abs(y[2]-y[1])
+
+    # iterate over the ψ array, fill it with random coordinates
+    for i = 1:n
+
+        # generate random coordinates that are evenly distributed in x and y direction
+        coords_rand = rand(Float64, 2)-0.5
+        x_temp = coords_rand[1] * span_x
+        y_temp = coords_rand[2] * span_y
+
+        # get the position of the random coordinates in the intensity array
+        m_x = round(Int, (x_temp-x[1]) / Δx, RoundDown)+1
+        m_y = round(Int, (y_temp-y[1]) / Δy, RoundDown)+1
+
+        # fill the ψ, p_ges and flat_int vectors
+        ψ[n] = [x_temp, y_temp, 0. 0.]
+        int_flat[n] = intensity[m_x, m_y]
+        p_ges[n] = momentum
+
     end
 
     # return the wanted struct
